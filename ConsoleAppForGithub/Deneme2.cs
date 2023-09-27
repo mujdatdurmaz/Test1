0429753f5f95ed402e9a7153433b80a8eeb9cd19
MujdatDurmaz <mujdatdurmaz@gmail.com>
2023-09-28 01:35:15 +0300

    deneme2

--- ConsoleAppForGithub/Deneme2.cs
+++ ConsoleAppForGithub/Deneme2.cs
@@ -1,1 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConsoleAppForGithub
+{
+    internal class Deneme2
+    {
+    }
+}
